72f601cb9d36d08c544e566d94af2892979d23cc
Ahmed <ahmedoun199@gmail.com>
2021-03-17 15:56:19 +0100

    sign up and login

---
+++ server.js
@@ -0,0 +1,113 @@
+var express = require('express')
+var app = express()
+
+var formidable = require('express-formidable')
+app.use(formidable())
+
+
+var mongodb = require('mongodb')
+var mongoClient = mongodb.MongoClient
+var ObjectId = mongodb.ObjectID
+
+
+var http = require('http').createServer(app)
+var bcrypt = require('bcrypt')
+var fileSystem = require('fs')
+
+var jwt = require('jsonwebtoken')
+var accessTokenSecret = "myAccesTokenSecret1234567890"
+
+app.use("/public",express.static(__dirname + "/public"))
+app.set("view engine", "ejs")
+
+var socketIO = require("socket.io")(http)
+var socketId=""
+var users = []
+
+var mainURL = "http://localhost:3000"
+socketIO.on("connection",function (socket) {
+    console.log("User connected" , socket.id)
+    socketId = socket.id ;
+})
+
+http.listen(3000 , () => {
+    console.log("server Started")
+
+    mongoClient.connect("mongodb://localhost:27017",function (error,client) {
+        var database = client.db("pub_soc_db")
+        console.log('database connected')
+
+        app.get("/signup", function (request,response) {
+            response.render("signup")
+
+        })
+
+        app.post('/signup', (req, res) => {
+            var name = req.fields.name ;
+            var username = req.fields.username;
+            var email = req.fields.email;
+            var password = req.fields.password;
+            var gender = req.fields.gender;
+
+            database.collection("users").findOne({
+                $or : [
+                    {
+                        "email" : email
+                    },
+                    {
+                        "username" : username
+                    }
+                ]},function (err,user) {
+                    if (user == null) {
+                        bcrypt.hash(password,10,function (err,hash) {
+                            database.collection("users").insertOne({
+                                "name" : name,
+                                "username" : username ,
+                                "email" : email,
+                                "password" : hash,
+                                "gender" : gender ,
+                                "profileImage" : "" ,
+                                "coverPhoto" : "" ,
+                                "dob" : "" ,
+                                "city" : "" ,
+                                "country" : "" ,
+                                "aboutMe" : "" ,
+                                "friends" : [] ,
+                                "pages" : [] ,
+                                "notifications" : [] ,
+                                "groups" : [] ,
+                                "posts" : []
+                            },function (err,data) {
+                                /* res.json({
+                                    "status" : "success" ,
+                                    "message" : "Signed up successfully , You can login now ..."
+                                }) */
+                                if (!error) {
+                                    res.redirect("/login")
+                                }
+                            })
+                    })
+                    
+                    }else{
+                        res.json({
+                            "status" : "error" ,
+                            "message" : "Email or username already exist ... "
+                        })
+                    }
+                }
+            )
+
+            app.get('/login', (request, response) => {
+                response.render("login")
+            });
+
+            app.post('/login', (request,response) => {
+                var email = req.fields.email;
+                var password = req.fields.password;
+                
+            });
+        });
+    })
+})
+
+
